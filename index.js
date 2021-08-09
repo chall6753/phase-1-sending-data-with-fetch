2f57c44022e242c65076c2271141da5ad9fa39b0
Charles <chall70101@gmail.com>
2021-08-09 14:23:42 -0600

    commit tree bfbeb5c29d6f9b0a928af76f7a29ff302bda1fde

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Add your code here
+
+function submitData(userName, userEmail){
+    const formData = {
+        name: userName,
+        email: userEmail
+        
+    }
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+    body: JSON.stringify(formData)
+    }
+
+
+     return fetch('http://localhost:3000/users', configurationObject)
+        .then(response => response.json())
+        .then(function(object){
+            let newID = object.id 
+            document.body.innerHTML = newID
+
+        })
+        .catch(function(error) {
+            let message = "Unauthorized Access"
+            document.body.innerHTML = message
+            
+          })
+        
+
+
+            
+    
+ 
+
+ 
+}
+
+
+
+
+
+
+
+
+
